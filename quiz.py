5289c276e42b5406aba3f10d52417966e520b43f
frantisekt <frankoba577@gmail.com>
2021-05-12 23:06:37 -0500

    Added functions to read the text files.

--- quiz.py
+++ quiz.py
@@ -1,5 +1,21 @@
 # THIS IS A PROGRAM TO CONSTRUCT A  SIMPLE SELECTION QUIZ
 
+filename1 = 'preguntas.txt' # VARIABLE FOR THE QUESTIONS FILE.
+filename2 = 'respuestas.txt'  # VARIABLE FOR THE ANSWER FILE.
+flag = True
+def leer_pregunta():  # FUNCTION TO READ THE QUESTION IN THE QUESTION FILE.
+    with open(filename1, 'r') as f_obj1:  
+        preguntas = f_obj1.readlines()
+
+def leer_respuesta():  # FUNCTION TO READ THE ANSWERS IN THE ANSWERS FILE.
+    with open(filename2, 'r') as f_obj2:
+        respuestas = f_obj2.readlines()
+
+while flag:
+
+    flag = False
+
+
 
 
 
