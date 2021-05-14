29e8be8cf6e139a0b2ef25461de301662da2860d
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2021-05-14 08:46:20 -0500

    Changed the name of the respuestas file to opciones. Simplified some lines of codes, changed some names and made some updates to comments to make them clearer.

--- quiz.py
+++ quiz.py
@@ -1,54 +1,59 @@
-# With this i call files in the folder
+# With this i call files in the folder.
 filename_1 = 'preguntas'
-filename_2 = 'respuestas'
+filename_2 = 'opciones'
 filename_3 = 'correctas'
 # c is a counter, it will be used to get the index of each set of Questions
-# and d will help to get Answers
-c = 0
-d = 0
-e = 0
-f = 0
-points = 0
+# and correct answers. d is a counter that shows the options you have. 
+# f is a counter that will count how many times the while loop it's been running
+# the loop needs to run as many times as questions there are. 
+c, d, f, points = 0, 0, 0, 0
 
 # With this i open the file and create lists with each line in file.
 with open(filename_1, 'r') as f_obj:
     contents = f_obj.readlines()
 
-# Same as before but with answers
+# Same as before but with options.
 with open(filename_2, 'r') as f_obj2:
     contents2 = f_obj2.readlines()
 
-# Same as before but with corrects
+# Same as before but with correct answers.
 with open(filename_3, 'r') as f_obj3:
     contents3 = f_obj3.readlines()
 
+# An input just to check if you want to exit or continue.
 start = input("Inserta 'q' para salir, enter para continuar:\n")
 
+# e is total of questions.
 e = len(contents)
 
+# As explained before, the loop needs to work as many times as questions there
+# are, so, f(times the loops has runned) needs to be less than e(total of Q's)
 while f < e:
     f += 1 
     if start == 'q':
         break
 
     else:
-        # I don't need a loop to show Qs since i'll be showing them one at a time,
-        # so when i add the counter, next time while loop will start with c = 1 and # so on
+        # I don't need a loop to show Qs since i'll be showing them one at a 
+        # time, so when i add the counter, next time while loop will start with 
+        # c = 1 and so on
         print(contents[c].rstrip())
-        # I need to create a for loop that can help me showing 3 first sets of answers, i can use a range for loop that starts on c = 0 and finishes on 
-        # d + 3, try to think an answer, there will be a problem when you try to sum
-        # the starting range, because c = c + 1, and the starting range will always # be 1
+        
+        # This loop is used to show each set of answers stored, they'll be 
+        # showing in sets of 3. 
         for j in range(d, d + 3):
             print(contents2[j])
 
         d += 3
-
+        # input written selection
         selection = input("Por favor, escribe la respuesta correcta: \n")
-
+        # contents3 index will be the same as Q's, since there are as many Q's
+        # as correct answers (A correct ans for each Q)
         if selection.lower() == contents3[c].rstrip():
             points += 1
         else:
             points += 0
+    # Since contents3 uses c as index, i need to add1 for the next run
     c += 1
 
 print("Tu total de puntos es: ", points)
